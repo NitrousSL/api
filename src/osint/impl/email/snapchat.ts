0ff507bc0c36247f97891af5f39d6039b640a66b
marcus <97414481+kiryano@users.noreply.github.com>
2024-01-03 23:17:38 -0800

    [+] added more social media modules
    [+] added more social media modules

---
+++ src/osint/impl/email/snapchat.ts
@@ -0,0 +1,49 @@
+import { Category } from "@osint/category";
+import { Module }   from "@osint/module";
+
+
+import axios        from "axios";
+
+export class Snapchat extends Module {
+
+    public static readonly meta = {
+        name: "snapchat",
+        category: Category.Email,
+        description: "Finds out if a given email address has a Snapchat account."
+    };
+
+    constructor() { super(Snapchat.meta); }
+
+    public async query(query: string): Promise<any> {
+        try {
+            const response = await axios.post('https://bitmoji.api.snapchat.com/api/user/find', 
+            
+            { 
+                email: query,
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            }
+            );
+            
+            const data = response.data;
+
+            const accountCheck = '{"account_type":"snapchat"}';
+
+
+            if (data && JSON.stringify(data).includes(accountCheck)) {
+                return { status: 200, data: true };
+            } else {
+                return { status: 400, data: null };
+            } 
+        } catch (error) {
+            console.error("Error querying Snapchat API:", error);
+
+            return { status: 500, data: null };
+        }
+    }
+}
+
+module.exports = new Snapchat ;
