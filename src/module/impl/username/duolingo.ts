d890183d9028a92bc70d5c2287f6a4d8379b3ae8
hxruhime <38597755+hxruhime@users.noreply.github.com>
2024-01-14 15:34:53 -0800

    add headers to duolingo username module

--- src/module/impl/username/duolingo.ts
+++ src/module/impl/username/duolingo.ts
@@ -21,7 +21,12 @@ export class Duolingo extends Module {
 
     public async query(query: string): Promise<any> {
 
-        const response = await axios.get(`https://www.duolingo.com/2017-06-30/users?username=${query}`);
+        const response = await axios.get(`https://www.duolingo.com/2017-06-30/users?username=${query}`, {
+            headers: {
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+            }
+        });
 
         const exists = response.data.users.length > 0;
 
