40dfac67051fea0cff290cfbfc40d39a9578e59d
hxruhime <38597755+hxruhime@users.noreply.github.com>
2024-01-14 16:01:38 -0800

    test new headers

--- src/module/impl/email/hulu.ts
+++ src/module/impl/email/hulu.ts
@@ -24,6 +24,8 @@ export class Hulu extends Module {
         const response = await axios.get(`https://signup.hulu.com/api/v3/accounts/status?email=${query}`, {
             headers: {
                 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'en-US,en;q=0.9',
                 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
             }
         });
