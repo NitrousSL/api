be6eaf799f395b928abfb0b9968553946e5cd5a2
hxruhime <38597755+hxruhime@users.noreply.github.com>
2024-01-15 18:46:19 -0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import fastify, { FastifyInstance } from 'fastify';
 import compress                     from "@fastify/compress";
 import helmet                       from "@fastify/helmet";
+import cors                         from "@fastify/cors";
 
 /////////////////////////////////////////////////////////////
 //
@@ -25,6 +26,8 @@ import rOSINT     from '@route/rOSINT';
 /////////////////////////////////////////////////////////////
 
 async function main(fastify: FastifyInstance) {
+
+    await fastify.register(cors, { origin: '*', });
 
     fastify.register(compress);
     fastify.register(helmet);
